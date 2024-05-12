22824656b5628a3a195db7cebc4381b9d34e7293
ykim879 <kimyiyun302@gmail.com>
2024-05-11 18:36:15 -0700

    format the main page

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,13 @@ import Sidebar from "./slidebar/slidebar";
 import Map from "./map/map";
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-between p-24">
-      <div className="w-1/6 max-h-5xl  h-full items-center justify-between bg-[#EAD8C0]">
-      <Sidebar />
+    <main style={{ display: 'flex', height: '100vh', padding: 0 }}>
+      <div style={{ width: '16.666%', height: '100%', backgroundColor: '#EAD8C0' }}>
+        <Sidebar />
       </div>
-      <Map/>
+      <div style={{ flexGrow: 1, padding: '10px', height: '100%' }}>
+        <Map />
+      </div>
     </main>
   );
 }
